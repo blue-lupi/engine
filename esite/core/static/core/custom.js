16730390f343c261865ce36862ebf21e166af0c8
kleberbaum <kleberbaum@erebos.xyz>
2020-04-30 18:03:15 +0200

    Add some custom css
    I thought some custom css would improve the UX.

---
+++ esite/core/static/core/custom.js
@@ -0,0 +1,4 @@
+$(function() {
+  console.log($(".wagtail-logo-container-inner"))
+    document.querySelector(".wagtail-logo-container-inner").innerHTML='<img class="wagtail-logo wagtail-logo__body" src="/static/core/wolf.svg" alt="">';
+});
---
+++ esite/core/wagtail_hooks.py
@@ -0,0 +1,16 @@
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.utils.html import format_html
+from wagtail.core import hooks
+
+@hooks.register("insert_global_admin_css", order=100)
+def global_admin_css():
+    # Add /static/css/custom.css to admin.
+    return format_html('<link rel="stylesheet" href="{}">', static("core/custom.css"))
+
+@hooks.register("insert_global_admin_js", order=100)
+def global_admin_js():
+    # Add /static/css/custom.css to admin.
+    return format_html('<script src="{}"></script>', static("core/custom.js"))
+
+# SPDX-License-Identifier: (EUPL-1.2)
+# Copyright © 2019 Werbeagentur Christian Aichner
