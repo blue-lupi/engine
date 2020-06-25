0f773e1276ec6125c9eab3e7e99d1626fb4bba85
kleberbaum <kleberbaum@erebos.xyz>
2020-06-25 11:20:02 +0200

    Update hompage model
    The requirements of the homepage
    models.py has been reworked.

--- esite/home/models.py
+++ esite/home/models.py
@@ -50,106 +50,106 @@ class Button(models.Model):
 
 #> Header
 class _H_HeroBlock(blocks.StructBlock):
-    slide_image = ImageChooserBlock(null=True, blank=False, help_text="Big, high resolution slider image")
-    slide_loadimage = blocks.BooleanBlock(null=True, blank=True, default=True, required=False, help_text="Whether or not to load the slide image from CMS (Unchecked is better for performance, only check if you want to test a new image)")
-    slide_button = SnippetChooserBlock(Button, null=True, blank=True, required=False, help_text="The button displayed at the frontpage slider")
+    slide_image = ImageChooserBlock(null=True, blank=False, required=False, help_text="Big, high resolution slider image")
+    slide_loadimage = blocks.BooleanBlock(null=True, blank=False, required=False, default=True, help_text="Whether or not to load the slide image from CMS (Unchecked is better for performance, only check if you want to test a new image)")
+    slide_button = SnippetChooserBlock(Button, null=True, blank=False, required=False, help_text="The button displayed at the frontpage slider")
 
 #> Why Section
 class Why_ColumnBlock(blocks.StructBlock):
-    Column_image = ImageChooserBlock(null=True, blank=False, help_text="Icon representating the below content")
-    Column_head = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="The bold header text at the frontpage slider")
-    Column_subhead = blocks.RichTextBlock(null=True, blank=False, help_text="The content of the frontpage slider element", classname="full")
-    Column_paragraph = blocks.RichTextBlock(null=True, blank=False, help_text="Formatted text", classname="full")
+    Column_image = ImageChooserBlock(null=True, blank=False, required=False, help_text="Icon representating the below content")
+    Column_head = blocks.CharBlock(null=True, blank=False, required=False, classname="full title", help_text="The bold header text at the frontpage slider")
+    Column_subhead = blocks.RichTextBlock(null=True, blank=False, required=False, help_text="The content of the frontpage slider element", classname="full")
+    Column_paragraph = blocks.RichTextBlock(null=True, blank=False, required=False, help_text="Formatted text", classname="full")
 
 class _S_WhyBlock(blocks.StructBlock):
-    why_head = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="Bold header text")
-    why_displayhead = blocks.BooleanBlock(null=True, blank=True, default=True, required=False, help_text="Whether or not to display the header")
+    why_head = blocks.CharBlock(null=True, blank=False, required=False, classname="full title", help_text="Bold header text")
+    why_displayhead = blocks.BooleanBlock(null=True, blank=False, required=False, default=True, help_text="Whether or not to display the header")
     why_Columns = blocks.StreamBlock([
-        ('why_Column', Why_ColumnBlock(null=True, blank=False, icon='fa-columns'))
-    ], null=True, blank=False, max_num=8)
+        ('why_Column', Why_ColumnBlock(null=True, blank=False, required=False, icon='fa-columns'))
+    ], null=True, blank=False, required=False, max_num=8)
 
 #> About Shop
 class Shop_PricingcardBlock(blocks.StructBlock):
-    shopcard_background = ColorBlock(null=True, blank=False, help_text="Select background color that contrasts text")
-    shopcard_title = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="Title of pricing card")
-    shopcard_description = blocks.RichTextBlock(null=True, blank=False, help_text="Description of offer", classname="full")
-    shopcard_price = blocks.DecimalBlock(null=True, blank=False, decimal_places=2, help="Price of the offer")
-    shopcard_sucessmsg = blocks.RichTextBlock(null=True, blank=False, help_text="Success message", classname="full")
-    shopcard_button = SnippetChooserBlock(Button, null=True, blank=True, required=False, help_text="Button displayed at the pricing-section")
+    shopcard_background = ColorBlock(null=True, blank=False, required=False, help_text="Select background color that contrasts text")
+    shopcard_title = blocks.CharBlock(null=True, blank=False, required=False, classname="full title", help_text="Title of pricing card")
+    shopcard_description = blocks.RichTextBlock(null=True, blank=False, required=False, help_text="Description of offer", classname="full")
+    shopcard_price = blocks.DecimalBlock(null=True, blank=False, required=False, decimal_places=2, help="Price of the offer")
+    shopcard_sucessmsg = blocks.RichTextBlock(null=True, blank=False, required=False, help_text="Success message", classname="full")
+    shopcard_button = SnippetChooserBlock(Button, null=True, blank=False, required=False, help_text="Button displayed at the pricing-section")
 
 class _S_ShopBlock(blocks.StructBlock):
-    #shop_background = ColorBlock(null=True, blank=False, help_text="Select background color that contrasts text")
-    shop_head = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="Bold header text")
-    shop_displayhead = blocks.BooleanBlock(null=True, blank=True, default=True, required=False, help_text="Whether or not to display the header")
+    #shop_background = ColorBlock(null=True, blank=False, required=False, help_text="Select background color that contrasts text")
+    shop_head = blocks.CharBlock(null=True, blank=False, required=False, classname="full title", help_text="Bold header text")
+    shop_displayhead = blocks.BooleanBlock(null=True, blank=False, required=False, default=True, help_text="Whether or not to display the header")
 
 #> About Section
 class About_CardBlock(blocks.StructBlock):
-    card_head = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="The bold header text at the frontpage slider")
-    card_paragraph = blocks.RichTextBlock(null=True, blank=False, help_text="Formatted text", classname="full")
+    card_head = blocks.CharBlock(null=True, blank=False, required=False, classname="full title", help_text="The bold header text at the frontpage slider")
+    card_paragraph = blocks.RichTextBlock(null=True, blank=False, required=False, help_text="Formatted text", classname="full")
 
 class _S_AboutBlock(blocks.StructBlock):
-    about_head = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="Bold header text")
-    about_displayhead = blocks.BooleanBlock(null=True, blank=True, default=True, required=False, help_text="Whether or not to display the header")
+    about_head = blocks.CharBlock(null=True, blank=False, required=False, classname="full title", help_text="Bold header text")
+    about_displayhead = blocks.BooleanBlock(null=True, blank=False, required=False, default=True, help_text="Whether or not to display the header")
     about_cards = blocks.StreamBlock([
-        ('aboutcard', About_CardBlock(null=True, blank=False, icon='fa-info'))
-    ], null=True, blank=False, max_num=6)
+        ('aboutcard', About_CardBlock(null=True, blank=False, required=False, icon='fa-info'))
+    ], null=True, blank=False, required=False, max_num=6)
 
 #> Instagram Section
 class _S_InstagramBlock(blocks.StructBlock):
-    instagram_id = blocks.CharBlock(null=True, blank=False, classname="full", help_text="Instagram-Account id")
-    instagram_pc = blocks.CharBlock(null=True, blank=False, classname="full", help_text="Instagram-Post count")
+    instagram_id = blocks.CharBlock(null=True, blank=False, required=False, classname="full", help_text="Instagram-Account id")
+    instagram_pc = blocks.CharBlock(null=True, blank=False, required=False, classname="full", help_text="Instagram-Post count")
 
 #> Steps Section
 class Steps_StepBlock(blocks.StructBlock):
-    step_image = ImageChooserBlock(null=True, blank=False, help_text="Image fitting this step")
-    step_head = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="Bold header text")
-    step_subhead = blocks.RichTextBlock(null=True, blank=False, help_text="Short introduction to the following paragraph", classname="full")
-    step_paragraph = blocks.RichTextBlock(null=True, blank=False, help_text="Step paragraph", classname="full")
+    step_image = ImageChooserBlock(null=True, blank=False, required=False, help_text="Image fitting this step")
+    step_head = blocks.CharBlock(null=True, blank=False, required=False, classname="full title", help_text="Bold header text")
+    step_subhead = blocks.RichTextBlock(null=True, blank=False, required=False, help_text="Short introduction to the following paragraph", classname="full")
+    step_paragraph = blocks.RichTextBlock(null=True, blank=False, required=False, help_text="Step paragraph", classname="full")
 
 class _S_StepsBlock(blocks.StructBlock):
-    steps_head = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="Bold header text")
-    steps_displayhead = blocks.BooleanBlock(null=True, blank=True, default=True, required=False, help_text="Whether or not to display the header")
+    steps_head = blocks.CharBlock(null=True, blank=False, required=False, classname="full title", help_text="Bold header text")
+    steps_displayhead = blocks.BooleanBlock(null=True, blank=False, required=False, default=True, help_text="Whether or not to display the header")
     steps_steps = blocks.StreamBlock([
-        ('step', Steps_StepBlock(null=True, blank=False, icon='fa-list-ol'))
-    ], null=True, blank=False, max_num=4)
+        ('step', Steps_StepBlock(null=True, blank=False, required=False, icon='fa-list-ol'))
+    ], null=True, blank=False, required=False, max_num=4)
 
 #> Trusted Section
 class Trusted_PartnerBlock(blocks.StructBlock):
-    partner_logo = ImageChooserBlock(null=True, blank=False, help_text="Image fitting this step")
-    partner_link = blocks.URLBlock(null=True, blank=True, help_text="Important! Format https://www.domain.tld/xyz")
+    partner_logo = ImageChooserBlock(null=True, blank=False, required=False, help_text="Image fitting this step")
+    partner_link = blocks.URLBlock(null=True, blank=False, required=False, help_text="Important! Format https://www.domain.tld/xyz")
 
 class _S_TrustedBlock(blocks.StructBlock):
     trusted_partner = blocks.StreamBlock([
-        ('partner', Trusted_PartnerBlock(null=True, blank=True, icon='fa-id-card'))
-    ], null=True, blank=False, max_num=4)
+        ('partner', Trusted_PartnerBlock(null=True, blank=True, required=True, icon='fa-id-card'))
+    ], null=True, required=False)
 
 class _S_SmallTrustedBlock(blocks.StructBlock):
     trusted_partner = blocks.StreamBlock([
-        ('partner', Trusted_PartnerBlock(null=True, blank=True, icon='fa-id-badge'))
-    ], null=True, blank=False)
+        ('partner', Trusted_PartnerBlock(null=True, blank=True, required=True, icon='fa-id-badge'))
+    ], null=True, required=False)
 
 class _S_SmallTrustedPBlock(blocks.StructBlock):
     trusted_paymentmethods = blocks.StreamBlock([
-        ('paymentmethods', Trusted_PartnerBlock(null=True, blank=True, icon='fa-id-badge'))
-    ], null=True, blank=False)
+        ('paymentmethods', Trusted_PartnerBlock(null=True, blank=True, required=True, icon='fa-id-badge'))
+    ], null=True, required=False)
 
 #> Instagram Section
 class _S_WolfBlock(blocks.StructBlock):
-    wolf_head = blocks.CharBlock(null=True, blank=False, classname="full", help_text="Bold header text")
-    wolf_subhead = blocks.RichTextBlock(null=True, blank=False, help_text="The content of the black wolf coffee intro", classname="full")
+    wolf_head = blocks.CharBlock(null=True, blank=False, required=False, classname="full", help_text="Bold header text")
+    wolf_subhead = blocks.RichTextBlock(null=True, blank=False, required=False, help_text="The content of the black wolf coffee intro", classname="full")
 
 #> Trusted Section
 class FAQ_QuestionBlock(blocks.StructBlock):
-    question_icon = blocks.CharBlock(null=True, blank=True, help_text="Font Awesome icon name (e.g. facebook-f) from https://fontawesome.com/icons?d=gallery&s=solid&m=free")
-    question_head = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="Bold header text")
-    question_paragraph = blocks.RichTextBlock(null=True, blank=False, help_text="Formatted text", classname="full")
-    question_link = blocks.URLBlock(null=True, blank=True, help_text="Important! Format https://www.domain.tld/xyz")
+    question_icon = blocks.CharBlock(null=True, blank=True, required=True, help_text="Font Awesome icon name (e.g. facebook-f) from https://fontawesome.com/icons?d=gallery&s=solid&m=free")
+    question_head = blocks.CharBlock(null=True, blank=False, required=False, classname="full title", help_text="Bold header text")
+    question_paragraph = blocks.RichTextBlock(null=True, blank=False, required=False, help_text="Formatted text", classname="full")
+    question_link = blocks.URLBlock(null=True, blank=True, required=True, help_text="Important! Format https://www.domain.tld/xyz")
 
 class _S_FAQBlock(blocks.StructBlock):
-    header = blocks.CharBlock(null=True, blank=False, classname="full title", help_text="Bold header text")
+    header = blocks.CharBlock(null=True, blank=False, required=False, classname="full title", help_text="Bold header text")
     questions = blocks.StreamBlock([
-        ('question', FAQ_QuestionBlock(null=True, blank=False, icon='fa-question'))
-    ], null=True, blank=False)
+        ('question', FAQ_QuestionBlock(null=True, blank=False, required=False, icon='fa-question'))
+    ], null=True, required=False)
 
 #> Homepage
 class HomePage(Page):
