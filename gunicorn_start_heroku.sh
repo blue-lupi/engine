04ce7f81902cac3ceb804f1f29c97774b9d0d534
Simon Prast <simon@pra.st>
2022-07-04 10:50:51 +0200

    Add UWSGI configuration to Heroku

--- gunicorn_start_heroku.sh
+++ gunicorn_start_heroku.sh
@@ -1,7 +1,16 @@
 python manage.py migrate
 
 # Create the default admin user on startup after applying migrations.
-python manage.py autocreate_user
+#  python manage.py autocreate_user
 
 # Heroku handles worker spawning and port binding for us.
-python -m gunicorn francy.asgi:application -k uvicorn.workers.UvicornWorker
+# python -m gunicorn francy.asgi:application -k uvicorn.workers.UvicornWorker
+
+uwsgi \
+    --env UWSGI_WSGI_FILE=esite/wsgi_production.py \
+    --processes=4 \
+    --harakiri=20 \
+    --max-requests=5000 \
+    --vacuum \
+    --http-auto-chunked \
+    --http-keepalive
