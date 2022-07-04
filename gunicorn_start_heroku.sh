97d727ae7c2fe9652eb4fde7a2686e98f407f32c
Simon Prast <simon@pra.st>
2022-07-04 11:13:46 +0200

    Remove uWSGI, add ASGI configuration
    I've already got ASGI to work once. I can't configure wsgi correctly.

--- gunicorn_start_heroku.sh
+++ gunicorn_start_heroku.sh
@@ -4,15 +4,4 @@ python manage.py migrate
 #  python manage.py autocreate_user
 
 # Heroku handles worker spawning and port binding for us.
-# python -m gunicorn francy.asgi:application -k uvicorn.workers.UvicornWorker
-
-uwsgi \
-    --socket=127.0.0.1:$PORT \
-    --buffer-size=32768 \
-    --env UWSGI_WSGI_FILE=esite/wsgi_production.py \
-    --processes=4 \
-    --harakiri=20 \
-    --max-requests=5000 \
-    --vacuum \
-    --http-auto-chunked \
-    --http-keepalive
+python -m gunicorn francy.asgi:application -k uvicorn.workers.UvicornWorker
